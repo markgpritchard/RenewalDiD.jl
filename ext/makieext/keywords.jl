07d4636106913a21280d961ea87ec1cccb80365f
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2025-08-07 15:17:33 +0100

    move `NOPLOTNAMES` into its own file

--- ext/makieext/keywords.jl
+++ ext/makieext/keywords.jl
@@ -321,29 +321,3 @@ function _selectkws(expectedarguments; skip=Symbol[], kwargs...)
     end
     return (; (kwkeys .=> kwvals)...)
 end
-
-## outputs from MCMC that are not plotted 
-const NOPLOTNAMES = [ 
-    "iteration", 
-    "chain", 
-    "lp", 
-    "n_steps", 
-    "is_accept", 
-    "acceptance_rate", 
-    "log_density", 
-    "hamiltonian_energy", 
-    "hamiltonian_energy_error", 
-    "max_hamiltonian_energy_error", 
-    "tree_depth", 
-    "numerical_error", 
-    "step_size", 
-    "nom_step_size"
-]
-
-function _plotnames(df) 
-    allnames = names(df)
-    plotnames = allnames[findall(x -> x ∉ NOPLOTNAMES, allnames)]
-    return plotnames 
-end
-
-_plotnames(df, vs) = _plotnames(df)[vs]
